41c3fb60e57461fa92613fadea939cc16ef6cf49
Megan <mko5258@psu.edu>
2023-10-05 01:48:25 -0400

    Started code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+// AERSP 424 Homework 1: Question 7
+// Goal: Write functions named gradient_weights and update_weights that
+// computes equations.
 
